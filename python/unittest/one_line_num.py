8071be9f49374c098d108f3518ebb822d2e87f5a
mddemarie <mddemarie@gmail.com>
2017-08-07 13:31:13 +0200

    refactoring of unit tests

--- python/unittest/one_line_num.py
+++ python/unittest/one_line_num.py
@@ -1,22 +1,16 @@
 def number_per_line(num):
     if type(num) == str:
         raise TypeError("You did not pass a number!")
-    elif num == 0:
-        raise ValueError("The number is zero! The number has to \
-        be higher than 1.")
-    elif num > 100:
-        raise ValueError("Wrong number! The number is higher than 100.")
-    elif num < 0:
-        raise ValueError("The number is negative! Choose a positive \
-        number between 1 and 100.")
-    elif num % 5 == 0 and num % 3 == 0:
+    if num <= 0 or num > 100:
+        raise ValueError("The number is not in range between 1 and 100.")
+    if num % 5 == 0 and num % 3 == 0:
         return "FizzBuzz"
-    elif num % 3 == 0:
+    if num % 3 == 0:
         return "fizz"
-    elif num % 5 == 0:
+    if num % 5 == 0:
         return "buzz"
-    elif num >= 1:
+    if num >= 1:
         return num
 
 
-number_per_line(4)
+number_per_line(2)
